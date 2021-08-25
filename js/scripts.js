e88011be03e983efe82d5caae569fba8ad0a7550
Paweł Ciszewski <kardahim@gmail.com>
2021-08-25 12:03:27 +0200

    finish

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,21 @@ for (var i = 0; i < questions.length; i++) {
 function drop() {
     var element = this.getElementsByTagName("p");
     var span = this.getElementsByTagName("span");
+
+    var elements = document.getElementsByTagName("p");
+    var spans = document.getElementsByClassName("quest");
+
     if (element[0].style.display == "block") {
         element[0].style.display = "none";
-        span[0].style.color = "hsl(237, 12%, 33%)";
         span[0].style.fontWeight = "400";
     }
     else {
-        var elements = document.getElementsByTagName("p");
-        var spans = document.getElementsByClassName("quest");
         for (var i = 0; i < elements.length; i++) {
             elements[i].style.display = "none";
-            spans[i].style.color = "hsl(237, 12%, 33%)";
             spans[i].style.fontWeight = "400";
         }
         element[0].style.display = "block";
-        span[0].style.color = "hsl(238, 29%, 16%)";
         span[0].style.fontWeight = "700";
+        span[0].disabled = true;
     }
 }
